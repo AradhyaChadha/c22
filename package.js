83d19a0f159d59b420ab3aabb8ffc51f96ef5813
AradhyaChadha <72021261+AradhyaChadha@users.noreply.github.com>
2020-11-05 13:21:56 +0530

    Add files via upload

---
+++ package.js
@@ -0,0 +1,32 @@
+class package{
+    constructor(x,y,width,height){
+        var options ={
+            restitution:1,
+            'friction':1.0,
+            'density':1.0
+        }
+    
+        this.body= Bodies.rectangle(x,y,width,height,options);
+        
+        this.width=width;
+
+        this.height=height;
+
+        World.add(world,this.body);
+    }
+     display(){
+      
+         var pos=this.body.position;
+         rectMode(CENTER);
+         rect(pos.x,pos.y,this.width,this.height)
+         push();
+         translate(pos.x, pos.y);
+         //rotate(angle);
+         strokeWeight(3);
+         stroke('blue')
+         fill('red')
+         rectMode(CENTER)
+         rect(0, 0, this.width, this.height);
+         pop();
+     }
+    }
